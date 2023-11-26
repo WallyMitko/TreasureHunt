c6d2852a402c02912679589fb28a78c0560565c3
wallymitko <cam@bourdeau.dev>
2023-11-26 10:43:40 -0500

    Initial Commit

---
+++ TreasureHunt/TreasureHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreasureHuntApp.swift
+//  TreasureHunt
+//
+//  Created by Cam on 2023-11-26.
+//
+
+import SwiftUI
+
+@main
+struct TreasureHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
